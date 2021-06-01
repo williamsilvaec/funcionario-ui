d1c4e7e7a399e72ae2c7e45c934958299ee84195
william.silva <william@viasoft.com.br>
2021-06-01 20:59:40 -0300

    ajustes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
