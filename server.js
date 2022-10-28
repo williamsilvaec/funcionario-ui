7bde5555bace964392999a5470b8efc130fc1120
william <williamsilvaec@gmail.com>
2022-10-28 13:46:29 -0300

    add express-force-ssl

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
+app.set('forceSSLOptions', {
+  enable301Redirects: true,
+  trustXFPHeader: false,
+  httpsPort: 443,
+  sslRequiredMessage: 'SSL Required.'
+});
+
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
