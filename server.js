30cdc3c510278d193825e9e15804dee60066e021
william <williamsilvaec@gmail.com>
2022-10-28 15:59:29 -0300

    config express-force-ssl

--- server.js
+++ server.js
@@ -11,8 +11,24 @@ app.set('forceSSLOptions', {
 });
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-app.get('/*', (req, res) => {
+
+/*app.get('/!*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});*/
+
+app.get('/*', (req, res, next) => {
+  console.log('1');
+  if (req.get('X-Forwarded-Proto')==='https' || req.hostname === 'localhost') {
+    //Serve Angular App by passing control to the next middleware
+    console.log('2');
+    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+    next();
+  } else if(req.get('X-Forwarded-Proto')!=='https' && req.get('X-Forwarded-Port')!=='443'){
+    console.log('3');
+    //Redirect if not HTTP with original request URL
+    res.redirect('https://' + req.hostname + req.url);
+  }
+
 });
 
 app.listen(process.env.PORT || 8080);
