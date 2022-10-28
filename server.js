457fce3149f15fad72b3a21833586e2589af71c1
william <williamsilvaec@gmail.com>
2022-10-28 16:05:44 -0300

    add express-sslify

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
 const express = require('express');
 const path = require('path');
+const enforce = require('express-sslify');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.set('forceSSLOptions', {
-  enable301Redirects: true,
-  trustXFPHeader: false,
-  httpsPort: 443,
-  sslRequiredMessage: 'SSL Required.'
-});
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-
-/*app.get('/!*', (req, res) => {
+app.use(enforce.HTTPS());
+app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-});*/
-
-app.get('/*', (req, res, next) => {
-  console.log('1');
-  if (req.get('X-Forwarded-Proto')==='https' || req.hostname === 'localhost') {
-    //Serve Angular App by passing control to the next middleware
-    console.log('2');
-    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-    next();
-  } else if(req.get('X-Forwarded-Proto')!=='https' && req.get('X-Forwarded-Port')!=='443'){
-    console.log('3');
-    //Redirect if not HTTP with original request URL
-    res.redirect('https://' + req.hostname + req.url);
-  }
-
 });
+
 
 app.listen(process.env.PORT || 8080);
